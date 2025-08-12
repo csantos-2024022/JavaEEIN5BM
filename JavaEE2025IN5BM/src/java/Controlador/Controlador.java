ca56ea3520db559afe51f46e25acecc83b6fc484
csantos-2024022 <csantos-2024022@kinal.edu.gt>
2025-08-12 11:52:35 -0600

    Feat: corregir errores

--- JavaEE2025IN5BM/src/java/Controlador/Controlador.java
+++ JavaEE2025IN5BM/src/java/Controlador/Controlador.java
@@ -9,57 +9,30 @@
    package Controlador;
 
    import java.io.IOException;
-
    import javax.servlet.ServletException;
-
    import javax.servlet.http.HttpServlet;
-
    import javax.servlet.http.HttpServletRequest;
-
    import javax.servlet.http.HttpServletResponse;
-
-   /**
-
-   *
-
-   * @author informatica
-
-   */
 
    public class Controlador extends HttpServlet {
 
        /**
-
         * Processes requests for both HTTP <code>GET</code> and <code>POST</code>
-
         * methods.
-
         *
-
         * @param request servlet request
-
         * @param response servlet response
-
         * @throws ServletException if a servlet-specific error occurs
-
         * @throws IOException if an I/O error occurs
-
         */
-
        protected void processRequest(HttpServletRequest request, HttpServletResponse response)
 
            throws ServletException, IOException {
-
            String menu = request.getParameter("menu");
-
            String accion = request.getParameter("accion");
-
            if (menu.equals("Principal")) {
-
            request.getRequestDispatcher("admin.jsp").forward(request, response);
-
            }
-
        }
 
        // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
--- JavaEE2025IN5BM/src/java/com/alanlacan/modelo/EmpleadoDAO.java
+++ JavaEE2025IN5BM/src/java/com/alanlacan/modelo/EmpleadoDAO.java
@@ -1,75 +1,42 @@
 package com.alanlacan.modelo;
 
 import com.alanlacan.config.Conexion;
-
 import com.alanlacan.modelo.Empleado;
-
 import java.sql.Connection;
-
 import java.sql.PreparedStatement;
-
 import java.sql.ResultSet;
-
 
 public class EmpleadoDAO {
 
    Conexion cn= new Conexion();
-
    Connection con;
-
    PreparedStatement ps;
-
    ResultSet rs;
-
    int resp;
 
    public Empleado validar(String emailEmpleado, String telefonoEmpleado){
-
        //Instanciar el objetdo de la entidad Empleado
-
        Empleado empleado = new Empleado();
-
        //Agregar una variable de tipo String para mostrar consulta de sql
-
        String sql = "select * from Empleados where emailEmpleado = ? and telefonoEmpleado = ?";
-
        try {
-
            con = cn.Conexion();
-
            ps = con.prepareCall(sql);
-
            ps.setString(1, emailEmpleado);
-
            ps.setString(2, telefonoEmpleado);
-
            rs = ps.executeQuery();
-
            while (rs.next()) {
-
                empleado.setCodigoEmpleado(rs.getInt("codigoEmpleado"));
-
                empleado.setNombreEmpleado(rs.getString("nombreEmpleado"));
-
                empleado.setApellidoEmpleado(rs.getString("apellidoEmpleado"));
-
                empleado.setEmailEmpleado(rs.getString("emailEmpleado"));
-
                empleado.setTelefonoEmpleado(rs.getString("telefonoEmpleado"));    
-
            }
-
        } catch (Exception e) {
-
            System.out.println("El usuario o contrasena son incorrectos");
-
            e.printStackTrace();
-
        }
-
        return empleado; //Empleado encontrado
-
    }
-
 }
  
